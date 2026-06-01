b64194d409c8616adf6be00f5d8ba8b9e442c4ad
zhouyue <njyuezhou@163.com>
2026-06-01 17:37:58 +0800

    openAI库调用deepseekAPI

---
+++ week2/testDeepseekAPI.py
@@ -0,0 +1,17 @@
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=os.environ.get("DEEPSEEK_API_KEY"), 
+    base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model = "deepseek-v4-pro",
+    messages = [
+        {'role': 'system', 'content': 'You are a helpful assistant'},
+        {'role': 'user', 'content': 'Hello'},
+        ],
+    stream = False
+)
+
+print(response.choices[0].message.content)
