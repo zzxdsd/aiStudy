6a85688cba00bb1357dea172dce9fdeeece0c91f
zhouyue <njyuezhou@163.com>
2026-06-08 16:10:58 +0800

    新增代码健壮性修改，考虑多个tool调用，并增加异常处理

--- week3/save_notes.py
+++ week3/save_notes.py
@@ -59,13 +59,12 @@ def load_note():
     return notes
 
 def save_note(f_para):
-    # content = f_para['content']
-    # tag = f_para['tag']
     try:
         notes = load_note()
     except Exception as e:
         print(f'读取笔记失败：{e}')
-        notes = []
+        #代码健壮性修改1：此时继续写笔记，如果本地本身有note.json，此时写入就是把之前的笔记全覆盖了，所以要中止
+        return f'保存笔记时读文件出错，错误原因：{e}' 
 
     notes.append(f_para)
     try:
@@ -99,23 +98,38 @@ while True:
         continue
 
     # 考虑模型返回存在多个tool_cslls
-    if message.tool_calls:
+    while message.tool_calls:
         messages.append(message)
         for tool in message.tool_calls:
             tool_id = tool.id
             f_name = tool.function.name
-            f_para = json.loads(tool.function.arguments)  #是个str
+            #代码健壮性修改2：虽然大模型通常返回合法 JSON，但网络或模型异常时可能拿到非 JSON 字符串，
+            # 直接 json.loads 会抛异常并中断整个对话循环。
+            try:
+                f_para = json.loads(tool.function.arguments)
+            except Exception as e:
+                messages.append({'role': 'tool', 'tool_call_id': tool_id, 'content': '工具参数错误'})
+                continue
             func = available_tools.get(f_name)
             if func:
                 results = func(f_para)
                 messages.append({'role': 'tool', 'tool_call_id': tool_id, 'content': results})
             else:
                 messages.append({'role': 'tool', 'tool_call_id': tool_id, 'content': f'未知工具: {f_name}'})
-        message = send_message(messages)
-        print(f'Model:\t{message.content}')
+        #代码健壮性修改3：考虑第二次调用模型失败
+        try:
+            message = send_message(messages)
+        except Exception as e:
+            print(f'调用工具后模型返回失败：{e}')
+            messages.append({'role': 'assistant', 'content': '抱歉，处理您的请求时遇到错误。如果刚进行了保存操作，可能已完成，请勿重复保存。'})
+            print(f'Model:\t{messages[-1]['content']}')
+            break
+    #代码健壮性修改4：加了一层守卫，若代码正常执行进入else，若while被break出来的话，证明本轮作废，没有assistant输出
     else:
         messages.append({'role': 'assistant', 'content': message.content})
         print(f'Model:\t{message.content}')
+
+# print(f'messages: {messages}')
     # print(f'模型返回为：{message}')
         
 
